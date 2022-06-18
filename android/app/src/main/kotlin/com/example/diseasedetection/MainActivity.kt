1e671d4683a5cf01f64be38fad4ce6fa5c911450
vinipai45 <vinipai45@gmail.com>
2022-06-18 13:11:41 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/diseasedetection/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.diseasedetection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'src/locations.dart' as locations;
+// import 'package:flutter/material.dart';
+// //import 'package:latlong/latlong.dart';
+// import 'package:flutter_map/flutter_map.dart';
+import 'package:http/http.dart' as http;
+// import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<MyApp> {
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+
+    setState(() {
+      for (final office in googleOffices.offices) {
+        print(office.name);
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Disease Locations'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(0, 0),
+            zoom: 2,
+          ),
+          markers: _markers.values.toSet(),
+        ),
+      ),
+    );
+  }
+}
